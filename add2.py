4fe2a83779a216588fc961c9827e8cac28c55828
Aaditi2505 <aadititweety2505@gmail.com>
2025-06-17 23:02:50 +0530

    Update add2.py

--- add2.py
+++ add2.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/17H7GeXLk5E9wbHMdY1tS4bPDVAQNMKMj
 """
 
-pip install streamlit pandas scikit-learn
 
 import streamlit as st
 import pandas as pd
